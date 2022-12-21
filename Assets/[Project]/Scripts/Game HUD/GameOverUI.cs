b129c2398cd0808cd72b9f1bba489bd33089ef28
Pedro Nascimento <pedrovitor.marques@gmail.com>
2022-12-21 11:03:05 -0300

    High Score System

--- Assets/[Project]/Scripts/Game HUD/GameOverUI.cs
+++ Assets/[Project]/Scripts/Game HUD/GameOverUI.cs
@@ -9,6 +9,8 @@ public class GameOverUI : MonoBehaviour
 {
     [SerializeField] private CanvasGroup canvasGroup;
     [SerializeField] private TMP_Text score;
+    [SerializeField] private TMP_Text highScore;
+    [SerializeField] private GameObject recordPanel;
     [Space(10)]
     [SerializeField] private StatsData stats;
 
@@ -19,5 +21,18 @@ public class GameOverUI : MonoBehaviour
         this.canvasGroup.blocksRaycasts = true;
 
         this.score.text = this.stats.starCount.ToString("000");
+
+        int highScoreValue = PlayerPrefs.GetInt("highScore", 0);
+        this.highScore.text = highScoreValue.ToString("000");
+
+        if (this.stats.starCount > highScoreValue)
+        {
+            PlayerPrefs.SetInt("highScore", this.stats.starCount);
+            recordPanel.SetActive(true);
+        }
+        else
+        {
+            recordPanel.SetActive(false);
+        }
     }
 }
